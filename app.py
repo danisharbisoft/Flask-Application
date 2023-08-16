18c9ce3a8fe5d6f31cb611508d3e3c333c16e55a
DanishAbbas <danish.abbas@arbisoft.com>
2023-08-16 11:38:55 +0500

    Added option to delete

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=True)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.String(5), default=datetime.utcnow().strftime('%Y-%m-%d | %H:%M'))
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+
+def __repr__(self):
+    return '<Task %r>' % self.id
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -31,6 +32,17 @@ def index():
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem with deleting the task"
 
 
 if __name__ == "__main__":
